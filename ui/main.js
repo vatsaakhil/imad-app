1f7c71da29a2d5e9688cbea2d74c2b4fab0faaf5
Akhil Vatsa <vatsaakhil@gmail.com>
2018-02-20 20:17:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,12 @@ console.log('Loaded!');
 var element=document.getElementById('main');
 //move img
 var img=document.getElementById('madi');
-img.onclick=function() {
-    img.style.marginLeft='100px';
+var marginLeft=0;
+
+    function moveRight(){
+        marginLeft=marginLeft+10;
+        img.style.marginLeft=marginLeft+"px";
+    }
+    img.onclick=function() {
+    var interval=setInterval(moveRight,100);
 };
