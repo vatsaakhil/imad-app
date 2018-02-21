5753e1164813a77c244b6b38537f521aa85d607e
Akhil Vatsa <vatsaakhil@gmail.com>
2018-02-21 21:37:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ button.onClick=function(){
       //makes request to counter endpoint
       //capture the rsponse and store in a var
  
-     counter++; //render the variable in correct span
+     counter=counter+1; //render the variable in correct span
      var span=document.getElementById('count');
      span.innerHTML=counter.toString();
 };
