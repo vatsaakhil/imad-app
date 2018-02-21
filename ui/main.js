1e0406114217c6bbfd3fea9c9d7e2b4273ac55ec
Akhil Vatsa <vatsaakhil@gmail.com>
2018-02-21 22:59:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,8 +27,7 @@ request.send(null);
 
 
 //submit name 
-var nameInput=document.getElementById("name");
-var name= nameInput.value;
+
 var submit=document.getElementById("submit_btn");
 submit.onclick=function(){
      var request = new XMLHttpRequest(); //make server request and send name
@@ -52,6 +51,8 @@ submit.onclick=function(){
             }
         }
     };
+    var nameInput=document.getElementById("name");
+var name= nameInput.value;
 request.open('GET','http://vatsaakhil.imad.hasura-app.io/submit-name?='+name,true);
 request.send(null);
   
