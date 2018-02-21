5e00fbe627b1d95ae718d7da118d9b0695c36192
Akhil Vatsa <vatsaakhil@gmail.com>
2018-02-21 22:48:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,6 +92,14 @@ app.get('/counter', function (req, res) {
   res.send(path.join(counter.toString()));
 }); //picks up th
 
+var names=[];
+app.get('/submit-name/', function (req, res) {   //URL submit-name?name-xxx
+    var name=req.query.name; //get the name from request
+    names.push(name);
+   
+  res.send(JSON.stringify(names));  // //JSON turns obj to strings yay!
+}); 
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 }); //picks up the css file 
@@ -101,13 +109,7 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 }); //picks up the img file
-var names=[];
-app.get('/submit-name/', function (req, res) {   //URL submit-name?name-xxx
-    var name=req.query.name; //get the name from request
-    names.push(name);
-   
-  res.send(JSON.stringify(names));  // //JSON turns obj to strings yay!
-}); 
+
 app.get('/:articlename', function (req, res) {
     var articlename=req.params.articlename;
   res.send(createtemp(articles[articlename]));
