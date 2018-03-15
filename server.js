489d6672baf3a64fea7c0a44c18c661c966f88d2
Akhil Vatsa <vatsaakhil@gmail.com>
2018-03-15 19:56:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,60 +1,46 @@
-var express = require('express'); /*importing software packages---- creates web server and handles http connection*/ 
-var morgan = require('morgan');  /*importing software packages--- outputs log for server */ 
-var path = require('path');  /*importing software packages*/ 
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
 
 var Pool=require('pg').Pool;
-
 var config={
-    user: 'vatsaakhil',
+    user:'vatsaakhil',
     database:'vatsaakhil',
     host:'db.imad.hasura-app.io',
-    port: '5432',
-    password: process.env.DB_PASSWORD
+    port:'80',
+    password:process.env.DB_PASSWORD
+    
 };
-
 var app = express();
 app.use(morgan('combined'));
 
 var articles={
- 'article-one' : {
+    'ArticleFour':{
+    title:'Welcome Bhavana Singh',
+    heading:'ArticleFour4',
+    date:'February 28, 2018',
+    content:`<p>First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.</p><p>First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.</p></div>
+            <div>Written By~</div>
+            <strong>Bhavana Singh</strong>`
+            },
+    'ArticleThree':{
+    title:' Nishtha Pandey',
+    heading:'ArticleThree3',
+    date:'February 25, 2018',
+    content:`<p>First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.</p><p>First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.</p></div>
+            <div>Written By~</div>
+            <strong>Nishtha Pandey</strong>
+            `}     
     
-    title: 'Article One | Akhil Vatsa',
-    heading:'Article One',
-    date:'19 Feb 2018',
-    content: ` <p> This is the content for the first article Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-            
-          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum </p>
-              
-          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum </p>
-        `
+};
+var ArticleOne={
+    title:'Welcome Bhavana Singh',
+    heading:'ArticleOne1',
+    date:'February 23, 2018',
+    content:`<p>First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.</p><p>First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.First way to depoly my page on server.</p></div>
+            <div>Written By~</div>
+            <strong>Bhavana Singh</strong>`
     
-},
-'article-two' : {
-    
-    title: 'Article Two | Akhil Vatsa',
-    heading:'Article Two',
-    date:'19 Feb 2018',
-    content: ` <p> This is the content for the first article Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-            
-          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum </p>
-              
-          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum </p>
-        `
-    
-},
-'article-three' : {
-    
-    title: 'Article Three | Akhil Vatsa',
-    heading:'Article Three',
-    date:'19 Feb 2018',
-    content: ` <p> This is the content for the first article Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-            
-          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum </p>
-              
-          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum </p>
-        `
-    
-}
 };
 function CreateTemplate(data){
 var title=data.title;
@@ -84,48 +70,65 @@ return htmlTemplate;
 }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-}); //picks up the file UI 
-
-var counter=0;
-app.get('/counter', function (req, res) {
-    counter++;
-  res.send(path.join(counter.toString()));
-}); //picks up th
-
-var names=[];
-app.get('/submit-name/', function (req, res) {   //URL submit-name?name-xxx
-    var name=req.query.name; //get the name from request
-    names.push(name);
-   
-  res.send(JSON.stringify(names));  // //JSON turns obj to strings yay!
-}); 
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-}); //picks up the css file 
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-}); //picks up the js script
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-}); //picks up the img file
-
-app.get('/:articlename', function (req, res) {
-    var articlename=req.params.articlename;
-  res.send(CreateTemplate(articles[articlename]));
-}); //gets article1 file
-
+});
 
 var pool=new Pool(config);
-app.get('/test-db', function(req,res){
-    pool.query('SELECT * FROM test', function(err,result){
-        if(err) {
+app.get('/test-db',function(req,res){
+    pool.query('SELECT * FROM author',function(err,result){
+        if(err){
             res.status(500).send(err.toString());
-        } else {
+        }
+        else{
             res.send(JSON.stringify(result));
         }
+        
     });
+    
 });
+app.get('/register.html',function(req,res){ //First way to get a response from a server
+   res.sendFile(path.join(__dirname, 'ui', 'register.html')); 
+});
+
+app.get('/ArticleTwo.html',function(req,res){//Second way to get a response from the server
+   res.send('This is second way to get our response from server');
+});
+
+app.get('/ArticleOne.html',function(req,res){//Third way to deploy page on web server
+   res.send(CreateTemplate(ArticleOne));
+});
+
+
+
+var names=[];
+app.get('/submit-name',function(req,res){
+    var name=req.query.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+    
+});
+
+//app.post('/submit-name', function(req, res) {
+  //  var name = req.query.name;
+    //console.log("post received: %s", name);
+//});
+
+app.get('article/:articleName',function(req,res){
+    var articleName = req.params.articleName;
+   res.send(CreateTemplate(articles[articleName]));
+});
+
+
+
+
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
