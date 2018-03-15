fb937aa18fd34c00ffedb8e047bc0f3d1c9fd860
Akhil Vatsa <vatsaakhil@gmail.com>
2018-03-15 20:03:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ app.get('/', function (req, res) {
 
 var pool=new Pool(config);
 app.get('/test-db',function(req,res){
-    pool.query('SELECT * FROM name',function(err,result){
+    pool.query('SELECT * FROM id',function(err,result){
         if(err){
             res.status(500).send(err.toString());
         }
