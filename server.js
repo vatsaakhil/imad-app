5cf2c7f323a31710a6930f0e32650d855511f44b
Akhil Vatsa <vatsaakhil@gmail.com>
2018-03-15 19:37:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -125,7 +125,13 @@ var config={
 };
 var pool=new Pool(config);
 app.get('/test-db', function(req,res){
-    pool.query('SELECT * FROM test', function)
+    pool.query('SELECT * FROM test', function(err,result){
+        if(err) {
+            res.status(500).send(err.toString());
+        } else {
+            res.send(JSON.stringify(result));
+        }
+    });
 });
 
 // Do not change port, otherwise your app won't run on IMAD servers
